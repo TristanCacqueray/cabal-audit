a0b68c812df5cb844ee4a3b3d9a5d1ed242588bd
Tristan Cacqueray <tdecacqu@redhat.com>
2023-08-01 02:37:26 +0000

    Build all hie file with nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,9 +9,47 @@
     let
       pkgs = hspkgs.pkgs;
 
-      haskellExtend = hpFinal: hpPrev: {
-        cabal-audit = hpPrev.callCabal2nix "cabal-audit" self { };
-      };
+      # Enable hie output for a ghc package
+      withHie = drv:
+        let
+          ndrv = pkgs.haskell.lib.appendBuildFlags drv
+            [ "--ghc-options=-fwrite-ide-info" ];
+        in ndrv.overrideAttrs (_: prevAttrs: {
+          installPhase = ''
+            ${prevAttrs.installPhase}
+
+            # Put the hie file next to the .hi
+            if [ -d "$out/lib/ghc-*/lib/*ghc*/$pkgId" ]; then
+              hie="$(realpath $out/lib/ghc-*/lib/*ghc*/$pkgId/)"
+              echo "Copying hie files to $hie..."
+              pushd dist/build/
+                for hiefile in $(${pkgs.findutils}/bin/find . -type f -name "*.hie"); do
+                  mkdir -p "$hie/$(dirname $hiefile)";
+                  cp "$hiefile" "$hie/$hiefile"
+                done
+              popd
+            fi
+          '';
+        });
+
+      # Call withHie for all package set attributes
+      withAllHie = packagesSet:
+        builtins.mapAttrs (name: value:
+          if builtins.elem name [ "mkDerivation" "ghc" ]
+          || !(builtins.isAttrs value) || !(builtins.hasAttr "pname" value) then
+            value
+          else
+            withHie value) packagesSet;
+
+      haskellExtend = hpFinal: hpPrev:
+        withAllHie hpPrev // ({
+          cabal-audit = hpPrev.callCabal2nix "cabal-audit" self { };
+          ghc = hpPrev.ghc.overrideAttrs (prev: {
+            patches = (hpPrev.ghc.patches or [ ])
+              ++ [ ./0001-Enable-hie-file-generation.patch ];
+          });
+        });
+
       hsPkgs = pkgs.hspkgs.extend haskellExtend;
 
       baseTools = with pkgs; [
@@ -25,6 +63,8 @@
     in {
       packages."x86_64-linux".default =
         pkgs.haskell.lib.justStaticExecutables hsPkgs.cabal-audit;
+      packages."x86_64-linux".test = hsPkgs.ghc-byteorder;
+      packages."x86_64-linux".ghc = hsPkgs.ghc;
       devShell."x86_64-linux" = hsPkgs.shellFor {
         packages = p: [ p.cabal-audit ];
         buildInputs = with pkgs; [ ghcid haskell-language-server ] ++ baseTools;
