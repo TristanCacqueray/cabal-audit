0fe6646d882aca6376453f262818719d2b587f71
Tristan Cacqueray <tdecacqu@redhat.com>
2023-08-05 20:21:36 +0000

    Apply formatting and add bin/run-tests helpers

--- cabal-audit-test/src/CabalAudit/Test/External.hs
+++ cabal-audit-test/src/CabalAudit/Test/External.hs
@@ -4,5 +4,5 @@ import Text.JSON qualified
 
 decodeFloat :: String -> Maybe Float
 decodeFloat str = case Text.JSON.decode str of
-  Text.JSON.Ok v -> Just v
-  Text.JSON.Error _ -> Nothing
+    Text.JSON.Ok v -> Just v
+    Text.JSON.Error _ -> Nothing
